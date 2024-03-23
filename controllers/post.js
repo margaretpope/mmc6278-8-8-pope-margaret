dc85ff48e5a87ec1512e02843a4f1bfe7ef2ca99
Margaret Pope <mmp298@gmail.com>
2024-03-23 12:02:52 -0500

    fixing heroku error

--- controllers/post.js
+++ controllers/post.js
@@ -16,7 +16,7 @@ async function create(req, res, next) {
 async function get(req, res) {
   try {
     const slug = req.params.slug
-    const post = await Post.findOne({slug}).populate({path:'tags'}).lean()
+    const post = await Post.findOne(slug).lean().populate('tags')
     // TODO: Find a single post
     // find a single post by slug and populate 'tags'
     // you will need to use .lean() or .toObject()
